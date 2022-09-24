55a0bedefd50c1256a6e660046724982a21bba00
Brad Johnson <2021bradley@gmail.com>
2022-09-24 11:39:59 -0500

    Removed mock data - use real data instead

--- client/index.js
+++ client/index.js
@@ -1,44 +1,49 @@
-// import { connect } from "./api";
-const socket = new WebSocket("ws://integri-scan.herokuapp.com/ws?roomID=1234&modality=XRAY");
+console.log("Attempting Connection...");
 
-// connect to websocket and handle messages
-const infoList = document.getElementById("info-list")
-const examOpts = document.getElementById("exam-opts")
-const excludedOpts = document.getElementById("excluded-opts");
+const socket = new WebSocket(
+  "ws://integri-scan.herokuapp.com/ws?roomID=1234&modality=XRAY"
+);
 
-let examCheckedCount = 0;
+socket.onopen = () => {
+  console.log("Successfully Connected");
+};
 
-socket.addEventListener('message', (event) => {
-  console.log('Message from server ', event.data);
+socket.onclose = (event) => {
+  console.log("Socket Closed Connection: ", event);
+};
 
-  infoList.innerHTML = '';
-  examOpts.innerHTML = '';
-  excludedOpts.innerHTML = '';
+socket.onerror = (error) => {
+  console.log("Socket Error: ", error);
+};
+
+// handle messages received from server
+socket.onmessage = (event) => {
+  const msg = JSON.parse(event.data);
+  console.log("Message received from server: ", msg);
+  switch (msg.Type) {
+    case 1:
+      break;
+    case 2:
+      updatePatientInfo(msg);
+      break;
+    default:
+      console.log(`Unknown message type ${msg.Type} received from server`);
+      break;
+  }
+};
+
+// update patient info displayed on page from message received from server
+function updatePatientInfo(msg) {
+  const infoList = document.getElementById("info-list");
+  const examOpts = document.getElementById("exam-opts");
+  const excludedOpts = document.getElementById("excluded-opts");
+
+  let examCheckedCount = 0;
+
+  infoList.innerHTML = "";
+  examOpts.innerHTML = "";
+  excludedOpts.innerHTML = "";
   examCheckedCount = 0;
-  // FIXME: Change back later to not hard-coded data
-  // const msg = JSON.parse(event.data);
-  const msg = {
-    "patient": {
-      "ID": 1293811,
-      "Name": "Brad J", 
-      "Birthdate": "07/18/2002", 
-      "Sex": "Male", 
-      "Diagnosis": "Skin Cancer"
-    },
-    "exams": {
-      "Angiography": 0,
-      "Arthrography": 0,
-      "Bone Density Scan": 1,
-      "Bone XRAY": 0,
-      "Chest XRAY": 1,
-      "Crystogram": 0,
-      "Fluoroscopy": 1,
-      "Mammography": 0,
-      "Myelography": 0,
-      "Skull Radiography": 0,
-      "Virtual Colonoscopy": 1
-    }
-  };
 
   for (let key in msg.patient) {
     const li = document.createElement("li");
@@ -49,37 +54,24 @@ socket.addEventListener('message', (event) => {
   for (let key in msg.exams) {
     const li = document.createElement("li");
     li.setAttribute("class", "list-group-item");
-    const isSuggested = (msg.exams[key] == 1 ? true : false);
+    const isSuggested = msg.exams[key] == 1 ? true : false;
     li.innerHTML = `
       <input
         class="form-check-input me-1"
         type="checkbox"
         value=""
-        ${(isSuggested ? 'checked=true' : '')}
+        ${isSuggested ? "checked=true" : ""}
         id="checkbox-${id}"
       />
       <label class="form-check-label" for="checkbox-${id}"
         >${key}</label
       >`;
-      if (isSuggested) {
-        examOpts.appendChild(li);
-        examCheckedCount = examCheckedCount + 1;
-      }
-      else {
-        excludedOpts.appendChild(li);
-      }
-    id++;
+    if (isSuggested) {
+      examOpts.appendChild(li);
+      examCheckedCount = examCheckedCount + 1;
+    } else {
+      excludedOpts.appendChild(li);
     }
-  switch (msg.Type) {
-    case 1:
-      console.log(msg)
-      break;
-    case 2:
-      console.log("Message to be processed", msg.Body)
-      break;
-    default:
-      // skip
-      break;
+    id++;
   }
-});
-
+}
