98c0d8e9464ca68e3e53808c1f1d1ce4792d11db
Brad Johnson <2021bradley@gmail.com>
2022-09-24 18:11:24 -0500

    Added location and epic spinner

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,16 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
 import React, { useState, useEffect } from "react";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import SuccessAnimation from "./SuccessAnimation.js";
+import * as Location from "expo-location";
 
 export default function App() {
-  // camera permissions
-  const [hasPermission, setHasPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasLocationPermission, setHasLocationPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [scanTimeout, setScanTimeout] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [showSuccess, setShowSuccess] = useState(false);
   const [patientID, setPatientID] = useState(null);
   const [open, setOpen] = useState(false);
@@ -25,45 +28,77 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === "granted");
+      let { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasCameraPermission(status === "granted");
+      status = await Location.requestForegroundPermissionsAsync();
+      setHasLocationPermission(status === "granted");
     })();
   }, []);
 
   const handleBarCodeScanned = async ({ type, data }) => {
-    setScanned(true);
-    setPatientID(data);
-    // send message to server with scanned patient ID
-    try {
-      await fetch(
-        `https://integri-scan.herokuapp.com/scan-exams?modality=${modality}&patientID=${data}`
+    if (scanned) return;
+    if (!modality) {
+      alert("Please select a modality before scanning.");
+      setScanTimeout(
+        setTimeout(() => {
+          setScanTimeout(null);
+        }, 3000)
       );
+      return;
+    }
+
+    setIsLoading(true);
+    setScanned(true);
+    try {
+      const location = await Location.getCurrentPositionAsync({
+        accuracy: 3,
+      });
+      console.log(location);
+      setPatientID(data);
+      console.log(`https://integri-scan.herokuapp.com/scan-exams
+          ?modality=${modality}
+          &patientID=${data}
+          &latitude=${location.coords.latitude}
+          &longitude=${location.coords.longitude}
+          &altitude=${location.coords.altitude}`);
+      //send message to server with scanned patient ID
+      await fetch(
+        `https://integri-scan.herokuapp.com/scan-exams?modality=${modality}&patientID=${data}&latitude=${location.coords.latitude}&longitude=${location.coords.longitude}&altitude=${location.coords.altitude}`
+      );
+      setIsLoading(false);
       setShowSuccess(true);
     } catch (err) {
       alert(
         "An error occurred when trying to send patient data. Please try again."
       );
-    }
+      setIsLoading(false);
+    } 
   };
-
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
 
   return (
     <View style={styles.container}>
       <View style={styles.infoText}>
         <Text>Please scan patient barcode</Text>
       </View>
-      <BarCodeScanner
-        onBarCodeScanned={
-          scanned || !modality ? undefined : handleBarCodeScanned
-        }
-        style={StyleSheet.absoluteFillObject}
-      />
+      {hasCameraPermission == null ? (
+        <Text>Requesting for camera permission</Text>
+      ) : hasCameraPermission == false ? (
+        <Text>No access to camera. Please go to app permission settings and allow camera permissions.</Text>
+      ) : (
+        <BarCodeScanner
+          onBarCodeScanned={scanTimeout != null ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      )}
+      {(scanned) && (
+        <View style={styles.scanAgainButton}>
+          <Button
+            title={"Tap to Scan Again"}
+            onPress={() => setScanned(false)}
+          />
+        </View>
+      )}
+      {isLoading && <ActivityIndicator color={"00f"} size="large"/>}
       {showSuccess && (
         <SuccessAnimation
           onAnimationEnd={() => {
@@ -73,12 +108,6 @@ export default function App() {
         />
       )}
       <View style={styles.selectModality}>
-        {scanned && (
-          <Button
-            title={"Tap to Scan Again"}
-            onPress={() => setScanned(false)}
-          />
-        )}
         <DropDownPicker
           open={open}
           value={modality}
@@ -108,6 +137,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-end",
+    marginBottom: 3,
+  },
+  scanAgainButton: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
   barCodeView: {
     width: "100%",
