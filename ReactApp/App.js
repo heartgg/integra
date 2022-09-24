26bf1fb7c913d3c3180fe38287b08fb62fa44ee0
Alexis Kaufman <alexiskaufman@gmail.com>
2022-09-24 00:52:03 -0500

    add dropdown menu to mobile app

--- ReactApp/App.js
+++ ReactApp/App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useState } from 'react';
+import ScanButton from './ScanButton.js';
 
 export default function App() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'IE Fluoro', value: 'IE Fluoro'},
+    {label: 'XRAY', value: 'XRAY'},
+    {label: 'CT', value: 'CT'},
+    {label: 'IR', value: 'IR'},
+    {label: 'MRI', value: 'MRI'},
+    {label: 'US', value: 'US'},
+    {label: 'Dexa', value: 'Dexa'},
+    {label: 'Nuc Med', value: 'Nuc Med'}
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
     </View>
   );
 }
